06c58d182d8a315f17e0608c774c7b885a933471
unknown <L221050155@pabellon.tecnm.mx>
2023-11-07 10:24:40 -0600

    ExamenU2

--- lista.py
+++ lista.py
@@ -1,14 +1,6 @@
 class Cola:
-    def __init__(self):
-        self.items = ["VANESSA", "EMMANUEL", "MELANY", "ARATH", "GAEL", "JULIAN", "SAJITH", "ZULEYMA", "AXEL", "FERNANADA", "JUAN", "AYLIN", "SANTIAGO", "JONATHAN", "XITLALI"]
-
-    def cola(self,x):
-        self.items.append(x)
     
     def colaF(self):
-        try:
-            return self.items.pop(0)
-        except:
             raise ValueError("La cola está vacía")
     def print_cola(self):
         print(self.items)
@@ -28,24 +20,23 @@ class Pila:
     def print_pila(self):
         print(self.items)
 
+nombres= ["VANESSA", "EMMANUEL", "MELANY", "ARATH", "GAEL", "JULIAN", "SAJITH", "ZULEYMA", "AXEL", "FERNANADA", "JUAN", "AYLIN", "SANTIAGO", "JONATHAN", "XITLALI"]
 
 #Ejemplo de uso 
 Mpila = Pila()
-Mpila.push("Zuleyma")
-Mpila.push("Xitlali")
-Mpila.push("Vanessa")
-Mpila.push("Santiago")
-Mpila.push("Sajith")
-Mpila.push("Melany")
-Mpila.push("Julian")
-Mpila.push("Juan")
-Mpila.push("Jonathan")
-Mpila.push("Gael")
-Mpila.push("Fernanda")
-Mpila.push("Emmanuel")
-Mpila.push("Aylin")
-Mpila.push("Axel")
-Mpila.push("Arath")
+Mpila.push(nombres[7])
+Mpila.push(nombres[14])
+Mpila.push(nombres[0])
+Mpila.push(nombres[12])
+Mpila.push(nombres[2])
+Mpila.push(nombres[10])
+Mpila.push(nombres[5])
+Mpila.push(nombres[4])
+Mpila.push(nombres[9])
+Mpila.push(nombres[1])
+Mpila.push(nombres[11])
+Mpila.push(nombres[8])
+Mpila.push(nombres[3])
 Mpila.print_pila()
 
 
